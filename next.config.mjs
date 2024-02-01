418c59894e645f12e6ad2523ccd5fe92e12f27d3
Israel Kilday <israel.kilday@yahoo.com.br>
2024-02-01 19:06:37 -0300

    feat: add footer component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "utfs.io",
+            },
+        ],
+    },
+};
+
 
 export default nextConfig;
