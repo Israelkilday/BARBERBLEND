5304529aa1aee2177b802277ad607fa8569a105f
Israel Kilday <israel.kilday@yahoo.com.br>
2024-02-02 20:24:57 -0300

    feat: add sigiIn and signOut

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,13 +3,17 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
-import { signIn } from "next-auth/react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 const Header = () => {
-    const handleLoginClick = async () => {
-        await signIn();
-    };
+    const { data, status } = useSession();
+
+    const handleLogoutClick = () => signOut();
+    const handleLoginClick = () => signIn("google");
 
     return (
         <Card>
@@ -17,14 +21,74 @@ const Header = () => {
 
                 <Image src="/Logo.png" alt="FSW Barber" height={22} width={120} />
 
-                {/* <Button variant="outline" size="icon" className="h-8 w-8">
-                    <MenuIcon size={18} />
-                </Button> */}
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon" className="h-8 w-8">
+                            <MenuIcon size={18} />
+                        </Button>
+                    </SheetTrigger>
 
-                <Button onClick={handleLoginClick}>Login</Button>
+                    <SheetContent className="p-0">
+                        <SheetHeader className="text-left border-b border-solid border-secondary p-5">
+                            <SheetTitle>Menu</SheetTitle>
+                        </SheetHeader>
+
+                        {data?.user ? (
+                            <div className="flex justify-between px-5 py-6 items-center">
+                                <div className="items-center gap-3">
+                                    <Avatar>
+                                        <AvatarImage src={data.user?.image ?? ""} />
+                                    </Avatar>
+
+                                    <h2 className="font-bold">{data.user.name}</h2>
+                                </div>
+
+                                <Button variant="secondary" size="icon">
+                                    <LogOutIcon onClick={handleLogoutClick} />
+                                </Button>
+                            </div>
+
+                        ) : (
+                            <div className="flex flex-col px-5 py-6 gap-3">
+                                <div className="flex items-center gap-2">
+                                    <UserIcon size={32} />
+
+                                    <h2 className="font-bold">Olá, faça seu login</h2>
+                                </div>
+
+                                <Button
+                                    variant="secondary"
+                                    onClick={handleLoginClick}
+                                    className="w-full justify-start"
+                                >
+                                    <LogInIcon className="mr-2" size={18} />
+                                    Fazer Login
+                                </Button>
+                            </div>
+                        )}
+
+                        <div className="flex flex-col gap-3 px-5">
+                            <Button variant="outline" className="justify-start" asChild>
+                                <Link href="/">
+                                    <HomeIcon size={18} className="mr-2" />
+                                    Inicio
+                                </Link>
+                            </Button>
+
+                            {data?.user && (
+                                <Button variant="outline" className="justify-start" asChild>
+                                    <Link href="/bookings">
+                                        <CalendarIcon size={18} className="mr-2" />
+                                        Agendamentos
+                                    </Link>
+                                </Button>
+                            )}
+                        </div>
+                    </SheetContent>
+                </Sheet>
             </CardContent>
         </Card>
     );
-}
+};
 
 export default Header;
