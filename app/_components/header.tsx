963316ac091b88466b1a1c83199ec44745746b79
Israel Kilday <israel.kilday@yahoo.com.br>
2024-02-08 20:31:09 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,32 +5,32 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-// import { signIn, signOut, useSession } from "next-auth/react";
-// import { Avatar, AvatarImage } from "./ui/avatar";
 import SideMenu from "./side-menu";
 import Link from "next/link";
 
 const Header = () => {
     return (
-        <Card>
-            <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Link href="/">
-                    <Image src="/Logo.png" alt="FSW Barber" height={22} width={120} />
-                </Link>
+        <header>
+            <Card>
+                <CardContent className="p-5 justify-between items-center flex flex-row">
+                    <Link href="/">
+                        <Image src="/Logo.png" alt="FSW Barber" height={22} width={120} />
+                    </Link>
 
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button variant="outline" size="icon" className="h-8 w-8">
-                            <MenuIcon size={18} />
-                        </Button>
-                    </SheetTrigger>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon" className="h-8 w-8">
+                                <MenuIcon size={18} />
+                            </Button>
+                        </SheetTrigger>
 
-                    <SheetContent className="p-0">
-                        <SideMenu />
-                    </SheetContent>
-                </Sheet>
-            </CardContent>
-        </Card>
+                        <SheetContent className="p-0">
+                            <SideMenu />
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
     );
 };
 
