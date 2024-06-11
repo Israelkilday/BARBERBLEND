796d9caa789df14e11b31bbb774e8331f29f2386
Israel Kilday <israel.kilday@yahoo.com.br>
2024-06-11 16:03:21 -0300

    feat: update header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,8 +1,15 @@
-"use client"
+"use client";
 
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { CalendarIcon, CircleUserRound, LogOutIcon, MenuIcon, Scissors, UserIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  CircleUserRound,
+  LogOutIcon,
+  MenuIcon,
+  Scissors,
+  UserIcon,
+} from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import SideMenu from "./side-menu";
 import Link from "next/link";
@@ -10,77 +17,83 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 
 const Header = () => {
-    const { data } = useSession();
+  const { data } = useSession();
 
-    const handleLogoutClick = () => signOut();
-    const handleLoginClick = () => signIn("google");
+  const handleLogoutClick = () => signOut();
+  const handleLoginClick = () => signIn("google");
 
-    return (
-        <header>
-            <Card>
-                <CardContent className="p-5 justify-between items-center flex flex-row lg:px-32">
-                    <Link href="/" className="flex items-center gap-1 p-2 rounded-md hover:bg-accent font-bold duration-300 outline-none">
-                        <Scissors className="font-bold text-purple-500" />
-                        
-                        <h2 className="md:text-lg font-berkshire-swash">BARBERBLEND</h2>
-                        
-                        <Scissors className="font-bold text-purple-500 trasform rotate-180" />
-                    </Link>
+  return (
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between items-center flex flex-row lg:px-32">
+          <Link
+            href="/"
+            className="flex items-center gap-1 p-2 rounded-md hover:bg-accent font-bold duration-300 outline-none"
+          >
+            <Scissors className="font-bold text-purple-500" />
 
-                    {data?.user ? (
-                        <div className="flex gap-2 justify-between px-5 lg:px-0 items-center">
-                            <div className="hidden items-center gap-3 lg:flex">
-                                <Avatar>
-                                    <AvatarImage src={data.user?.image ?? ""} />
-                                </Avatar>
+            <h2 className="md:text-lg font-berkshire-swash">BARBERBLEND</h2>
 
-                                <h2 className="font-bold md:text-lg">{data.user.name}</h2>
+            <Scissors className="font-bold text-purple-500 trasform rotate-180" />
+          </Link>
 
-                                <Button
-                                    asChild
-                                    className="justify-start bg-card border-none hidden lg:flex hover:bg-accent"
-                                >
-                                    <Link href="/bookings" className="md:text-lg">
-                                        <CalendarIcon size={24} className="mr-2" />
-                                        Agendamentos
-                                    </Link>
-                                </Button>
+          {data?.user ? (
+            <div className="flex gap-2 justify-between px-5 lg:px-0 items-center">
+              <div className="hidden items-center gap-3 lg:flex">
+                <Avatar>
+                  <AvatarImage src={data.user?.image ?? ""} />
+                </Avatar>
 
-                                <Button
-                                    variant="secondary"
-                                    size="icon"
-                                    className="hidden lg:flex"
-                                >
-                                    <LogOutIcon onClick={handleLogoutClick} />
-                                </Button>
-                            </div>
-                        </div>
+                <h2 className="font-bold md:text-lg">{data.user.name}</h2>
 
-                    ) : (
-                        <Button
-                            onClick={handleLoginClick}
-                            className="hover:bg-primary/85 hidden lg:flex md:text-lg"
-                        >
-                            <CircleUserRound className="mr-2" size={24} />
-                            Login
-                        </Button>
-                    )}
+                <Button
+                  asChild
+                  className="justify-start bg-card border-none hidden lg:flex hover:bg-accent"
+                >
+                  <Link href="/bookings" className="md:text-lg">
+                    <CalendarIcon size={24} className="mr-2" />
+                    Agendamentos
+                  </Link>
+                </Button>
 
-                    <Sheet>
-                        <SheetTrigger asChild>
-                            <Button variant="outline" size="icon" className="h-8 w-8 md:h-9 md:w-9 lg:hidden">
-                                <MenuIcon size={26} className="md:block" />
-                            </Button>
-                        </SheetTrigger>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="hidden lg:flex"
+                >
+                  <LogOutIcon onClick={handleLogoutClick} />
+                </Button>
+              </div>
+            </div>
+          ) : (
+            <Button
+              onClick={handleLoginClick}
+              className="hover:bg-primary/85 hidden lg:flex md:text-lg"
+            >
+              <CircleUserRound className="mr-2" size={24} />
+              Login
+            </Button>
+          )}
 
-                        <SheetContent className="p-0">
-                            <SideMenu />
-                        </SheetContent>
-                    </Sheet>
-                </CardContent>
-            </Card>
-        </header>
-    );
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 md:h-9 md:w-9 lg:hidden"
+              >
+                <MenuIcon size={26} className="md:block" />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
 };
 
 export default Header;
--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -5,52 +5,52 @@ import Header from "../_components/header";
 import Search from "../(home)/_components/search";
 
 interface BarbershopsPageProps {
-    searchParams: {
-        search: string;
-    }
+  searchParams: {
+    search: string;
+  };
 }
 
 const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
-    if (!searchParams.search) {
-        return redirect("/");
-    }
+  if (!searchParams.search) {
+    return redirect("/");
+  }
 
-    const barbershops = await db.barbershop.findMany({
-        where: {
-            name: {
-                contains: searchParams.search,
-                mode: "insensitive",
-            }
-        }
-    })
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams.search,
+        mode: "insensitive",
+      },
+    },
+  });
 
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <div className="px-5 lg:px-0 py-6 flex flex-col gap-4">
-                <div className="md:w-1/2 lg:w-1/2 lg:px-32 lg:pt-5 ">
-                    <Search
-                        defaultValues={{
-                            search: searchParams.search,
-                        }}
-                    />
-                </div>
+      <div className="px-5 lg:px-0 py-6 flex flex-col gap-4">
+        <div className="md:w-1/2 lg:w-1/2 lg:px-32 lg:pt-5 ">
+          <Search
+            defaultValues={{
+              search: searchParams.search,
+            }}
+          />
+        </div>
 
-                <h1 className="text-gray-400 font-bold text-xs md:text-lg lg:text-xl uppercase lg:px-32 lg:pt-5">
-                    Resultados para &quot;{searchParams.search}&quot;
-                </h1>
+        <h1 className="text-gray-400 font-bold text-xs md:text-lg lg:text-xl uppercase lg:px-32 lg:pt-5">
+          Resultados para &quot;{searchParams.search}&quot;
+        </h1>
 
-                <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:px-32 lg:pt-5 lg:mb-20">
-                    {barbershops.map((barbershop) => (
-                        <div key={barbershop.id} className="md:w-full">
-                            <BarbershopItem barbershop={barbershop} />
-                        </div>
-                    ))}
-                </div>
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:px-32 lg:pt-5 lg:mb-20">
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className="md:w-full">
+              <BarbershopItem barbershop={barbershop} />
             </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BarbershopsPage;
