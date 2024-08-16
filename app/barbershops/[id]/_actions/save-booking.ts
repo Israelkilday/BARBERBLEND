2a495c724c8ea56c62f6b9705a9fecc0aa5b3efc
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-15 21:12:14 -0300

    feat: ensure user be autenticaded

--- app/barbershops/[id]/_actions/save-booking.ts
+++ app/barbershops/[id]/_actions/save-booking.ts
@@ -1,26 +1,33 @@
-"use server"
+"use server";
 
+import { authOptions } from "@/app/_lib/auth";
 import { db } from "@/app/_lib/prisma";
+import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
 
 interface SaveBookingParams {
-    barbershopId: string;
-    serviceId: string;
-    userId: string;
-    date: Date;
+  barbershopId: string;
+  serviceId: string;
+  userId: string;
+  date: Date;
 }
 
 export const saveBooking = async (params: SaveBookingParams) => {
-    await db.booking.create({
-        data: {
-            serviceId: params.serviceId,
-            userId: params.userId,
-            date: params.date,
-            barbershopId: params.barbershopId
-        },
-    });
+  const user = await getServerSession(authOptions);
 
-    revalidatePath("/")
-    revalidatePath("/bookings")
+  if (!user) {
+    throw new Error("Usuario não autentícado");
+  }
+
+  await db.booking.create({
+    data: {
+      serviceId: params.serviceId,
+      userId: params.userId,
+      date: params.date,
+      barbershopId: params.barbershopId,
+    },
+  });
+
+  revalidatePath("/");
+  revalidatePath("/bookings");
 };
-
